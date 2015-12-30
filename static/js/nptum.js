a6111ffd345cf3a584a7e9362f340a0f4dabbd8b
Chaoyi Zha <summermontreal@gmail.com>
2015-12-30 00:40:46 -0500

    Attempt to correctly align rows for cards

--- static/js/nptum.js
+++ static/js/nptum.js
@@ -2,18 +2,52 @@
     nptum core js
 */
 
-// load Handlebars templates
+// Set constants
+const A_MAX_ITEMS_PER_ROW = 2;
+
+// :oad Handlebars templates
 var cards_hbs   = $("#cards-template").html();
 var cards = Handlebars.compile(cards_hbs);
 
-// var rows_hbs   = $("#rows-template").html();
-// var rows = Handlebars.compile(rows_hbs);
+var modals_hbs   = $("#modals-template").html();
+var modals = Handlebars.compile(modals_hbs);
 
+var rows_hbs   = $("#rows-template").html();
+var rows = Handlebars.compile(rows_hbs);
 
+var cards_elem = $('#cards');
+
+function openNewNoteModal() {
+    var html = modals();
+    $('body').append(html);
+    $('#new-item-modal').openModal();
+
+    // $('#new-item-modal').remove();
+}
+
+function deleteCard(id) {
+    $('#'+id).remove();
+    var cards_last_elem = cards_elem.last();
+    if (cards_last_elem.size() === 0) {
+        cards_last_elem.remove();
+    }
+}
+
+function createNewRow() {
+    var new_row = rows();
+    cards_elem.append(new_row);
+}
 
 function loadCard(title, body, actions) {
     // actions are optional
     if (typeof actions === "undefined") {actions = "";}
+
+    var last_row = $('.row').last();
+
+    if ( (!last_row.hasClass('row') ) || (last_row.size() > A_MAX_ITEMS_PER_ROW) ) {
+        // no rows left, create new row
+        createNewRow();
+    }
 
     var context = {
         card_title: title,
@@ -21,21 +55,35 @@ function loadCard(title, body, actions) {
         card_actions: actions
     };
     var html = cards(context);
-    $("#cards").append(html);
+
+    last_row.append(html);
 }
 
-loadCard("hello", `I am a very simple card.
-I am good at containing small bits of information.
-I am convenient because I require little markup to use effectively.`);
+loadCard("Note to selfa", `Note to self:<ul>
+<li>Learn Rust</li>
+<li>Learn Iron</li>
+<li>Learn how to use Sessions with Iron</li>
+<li>Fix the issue with rows not being same size</li>
+<li>ffff</li>
+</ul>`);
 
-loadCard("hello", `I am a very simple card.
-I am good at containing small bits of information.
-I am convenient because I require little markup to use effectively.`);
+loadCard("Note to self", `Note to self:<ul>
+<li>Learn Rust</li>
+<li>Learn Iron</li>
+<li>Learn how to use Sessions with Iron</li>
+<li>Fix the issue with rows not being same size</li>
+</ul>`);
 
-loadCard("hello", `I am a very simple card.
-I am good at containing small bits of information.
-I am convenient because I require little markup to use effectively.`);
+loadCard("Note to self", `Note to self:<ul>
+<li>Learn Rust</li>
+<li>Learn Iron</li>
+<li>Fix the issue with rows not being same size</li>
+<li>Learn how to use Sessions with Iron</li>
+ddddddddddddd
+</ul>`);
 
-loadCard("hello", `I am a very simple card.
-I am good at containing small bits of information.
-I am convenient because I require little markup to use effectively.`);
+loadCard("Note to self", `Note to self:<ul>
+<li>Learn Rust</li>
+<li>Learn Iron</li>
+<li>Learn how to use Sessions with Iron</li>
+</ul>`);
